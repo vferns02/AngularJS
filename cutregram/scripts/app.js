213452c786ccb58e1459290ac5b4d322012ccdc8
vferns02 <vferns02@gmail.com>
2015-11-13 08:24:42 +0100

    AngularJS - Constants

--- cutregram/scripts/app.js
+++ cutregram/scripts/app.js
@@ -3,12 +3,12 @@ angular.module("cutregram",["ngRoute"]);
 
 //En fase de config, inyectamos $httpProvider para configurar las cabeceras por defecto de los distintos
 //metodos HTTP del servicio $http, q usamos para pedir los datos al servido
-angular.module("cutregram").config(function($httpProvider){
+angular.module("cutregram").config(function($httpProvider,Properties){
 
     //Configuramos el servicio $http para que envie la cabecera necesaria.
     // configurar http para que todas las peticiones qu hacems a traves de ese servicio metan la cabecera api-key
     $httpProvider.defaults.headers.common = {
-        "X-Cutregram-Api-Key":"e196f8ae74494e1d9f4635274dcb0a7a"
+        "X-Cutregram-Api-Key":Properties.apiKey
     };
 
     //Configuramos las cabeceras por defecto para evitar problemas de CORS. (cruce de dominios)
@@ -20,7 +20,7 @@ angular.module("cutregram").config(function($httpProvider){
 
 //En fase de config inyectamos $routeProvider para configurar las rutas de la app.
 
-angular.module("cutregram").config(function($routeProvider){
+angular.module("cutregram").config(function($routeProvider,Properties){
 
     //Voy a definir la ruta de "Todos los Posts"
     $routeProvider.when("/todos",{
@@ -30,7 +30,9 @@ angular.module("cutregram").config(function($routeProvider){
         //Tenemos que usar la anotacion de array en linea.
        resolve:{                          //Lo que hay en el resolve se ejecuta antes de la navegacion
            Posts: ["$http",function($http){
-               return $http.get("http://cutregram-sp.appspot.com/api/1/posts");
+               return $http.get(Properties.backendUrl + "/posts",{
+                   cache:true
+               });
            }]
        }
     });
@@ -44,7 +46,9 @@ angular.module("cutregram").config(function($routeProvider){
         //Tenemos que usar la anotacion de array en linea.
        resolve:{
            Posts:["$http", function($http){
-               return  $http.get("http://cutregram-sp.appspot.com/api/1/posts/me");
+               return  $http.get(Properties.backendUrl + "/posts/me",{
+                   cache:true
+               });
            }]
        }
     });
---
+++ cutregram/scripts/constants/Properties.js
@@ -0,0 +1,8 @@
+
+//En los constants y values definimos las propiedades de la aplicacion
+//Usamos constant en este caso porq necesito inyectar en fase de config.
+
+angular.module("cutregram").constant("Properties",{
+   apiKey:"e196f8ae74494e1d9f4635274dcb0a7a",
+    backendUrl:"http://cutregram-sp.appspot.com/api/1"
+});
